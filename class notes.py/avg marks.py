15d4097aeb100b47389268f799852e6e1d34f5fb
ragul66 <114981373+ragul66@users.noreply.github.com>
2023-05-27 19:33:37 +0530

    logicalconditions

--- class notes.py/avg marks.py
+++ class notes.py/avg marks.py
@@ -14,13 +14,32 @@ for i in range(5):
 avg=tot/5
 print("Average mark=",avg)
 
+#Example 1
+
+# Enter Marks Obtained in 5 Subjects: 
+# 89
+# 78
+# 67
+# 78
+# 78
+# Average mark= 78.0
+
+#Example 2
+# Enter Marks Obtained in 5 Subjects: 
+# 56
+# 67
+# 45
+# 78
+# 98
+# Average mark= 68.8
+
 #using while loop
 total = 0
 noOfStudents = 5
 i = 0
 while (i < noOfStudents):
-     mark=input("Enter the mark")
-     total += mark[i]
-i += 1
-print ("Avg is ",total)
+     mark=float(input("Enter the marks you got"))
 
+     total += mark
+     i += 1
+print ("Avg is ",)
