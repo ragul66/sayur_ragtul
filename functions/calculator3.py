72adca12aedb5f98a8473e6f6dc98b6a4d9e826d
ragul66 <114981373+ragul66@users.noreply.github.com>
2023-05-29 13:16:30 +0530

    functions finished

---
+++ functions/calculator3.py
@@ -0,0 +1,58 @@
+#Fix the syntax errors as needed.
+
+#This is code has functions for basic arithmetic operations. Add, subtract, multiply and divide
+#add new functions as needed.
+
+#The main functions is to find the total profit
+
+def addTwoNumbers(n1, n2):
+    ans = n1 + n2
+    return ans
+
+def subtractAfromB(a, b):
+    ans=b-a#FillinMissingCode
+    return ans#FillinMissingCode
+
+def multiplyTwoNumbers(n1,n2):
+    ans=n1*n2#FillinMissingCode
+    return ans#FillinMissingCode
+
+
+def divideAFromB(a,b):#FillinMissingCode):
+    ans=b-a 
+    return ans#FillinMissingCode
+
+#main code
+
+
+#we have sales data for a week. 
+costOfCoffee, costOfTea, costOfVadai = 25,20,25
+
+coffeeSales = [56,78,56,45,90, 103,120]
+teaSales = [100,123,456,123,222,400,346]
+vadaiSales = [23,45,67,12,89,90,120]
+
+#Find total sales in the week.
+#calculate avg sales for a week
+employeeSalary = 500 #Rs500 per day
+
+#calculate sales per week
+totalsales=addTwoNumbers(addTwoNumbers(sum(coffeeSales),sum(teaSales)),sum(vadaiSales))
+
+#calcuate sales per month
+salespermonth=multiplyTwoNumbers(totalsales,4)
+
+#calculate profit.
+profit=subtractAfromB(salespermonth,employeeSalary)
+
+
+
+ 
+
+
+
+#Call divide function to get the average from all three subjects
+avg=divideAFromB(3,totalsales)
+#FillinMissingCode
+
+print("The avg sales is ", avg)
