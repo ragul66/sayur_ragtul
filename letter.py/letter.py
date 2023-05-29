72653705015dbfa0518c2191eca60a91c8ff0497
ragul66 <114981373+ragul66@users.noreply.github.com>
2023-05-29 22:33:11 +0530

    sunday hw finished

---
+++ letter.py/letter.py
@@ -0,0 +1,17 @@
+"""Write a Python program to create a file where all letters of English alphabet are listed by specified 
+number of letters on each line.
+"""
+def create_alphabet_file(letters_per_line, file_name):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    with open(file_name, 'w') as file:
+        for i in range(0, len(alphabet), letters_per_line):
+            line = alphabet[i:i+letters_per_line]
+            file.write(line + '\n')
+
+    print(f"The file '{file_name}' has been created.")
+
+
+# Example usage
+letters_per_line = 1
+file_name = 'alphabet.txt'
+create_alphabet_file(letters_per_line, file_name)
